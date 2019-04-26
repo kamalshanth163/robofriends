da3a97a22aa9867306358e91cd5e820a9cd8e35f
kamalshanth163 <kamalshanthselvarajah163@gmail.com>
2019-04-26 19:08:27 +0530

    Updates

---
+++ precache-manifest.65016d7c8beacd44b92a362583343135.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "ee3f419f0a5b8e2f9804",
+    "url": "/robofriends/static/js/main.ec173ea9.chunk.js"
+  },
+  {
+    "revision": "a6c562d39718980ead20",
+    "url": "/robofriends/static/js/2.0ef9ac0a.chunk.js"
+  },
+  {
+    "revision": "ee3f419f0a5b8e2f9804",
+    "url": "/robofriends/static/css/main.c50eae0e.chunk.css"
+  },
+  {
+    "revision": "a6c562d39718980ead20",
+    "url": "/robofriends/static/css/2.990ad635.chunk.css"
+  },
+  {
+    "revision": "42b89a9a08026e51d08310d2f75f7352",
+    "url": "/robofriends/index.html"
+  }
+];
